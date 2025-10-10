3fe36f3fcc2e0f37be2c35783244e349f075696f
Doug Hamilton <Dmhamilton1@live.com>
2025-10-10 10:52:29 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,8 @@
 window.SUPABASE_URL = "https://hvmotpzhliufzomewzfl.supabase.co";
 window.SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2bW90cHpobGl1ZnpvbWV3emZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1NzY2NDUsImV4cCI6MjA1ODE1MjY0NX0.foHTGZVtRjFvxzDfMf1dpp0Zw4XFfD-FPZK-zRnjc6s";
+
+export const APP_CONFIG = {
+  mockMode: false,
+  enableAnalytics: false,
+};
